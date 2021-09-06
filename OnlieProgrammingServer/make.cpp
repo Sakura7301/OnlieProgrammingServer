206ad5d82becf7d889d445a1a2f5dac5be0eca92
Sakura7301 <2822344101@qq.com>
2021-09-06 09:38:46 +0800

    python语言现已支持

--- OnlieProgrammingServer/make.cpp
+++ OnlieProgrammingServer/make.cpp
@@ -96,8 +96,9 @@ int make_test(struct bufferevent *bev, int language)
 
 	if (language == 3)
 	{
-		char data[50] = "Python is not currently supported,Stay tuned.";
-		bufferevent_write(bev, data, strlen(data));//python暂不支持
+		char data[50] = "The results of the program are as follows:\n";
+		bufferevent_write(bev, data, strlen(data));//python无需编译,直接让out输出即可
+		return 3;
 	}
 
 	return -1;
@@ -154,5 +155,30 @@ int out_test(struct bufferevent *bev, int language)
 		return -1;
 	}
 
+	if (language == 3)
+	{
+		string name("touch ");
+		name = name + "out";
+		system(name.c_str());
+		system("python3 test.py > out 2>&1");
+		system("cat out");
+		char buf[BUFFSIZE];
+		memset(buf, '\0', sizeof(buf));
+		ifstream ifs;
+		ifs.open("out", ios::in);
+		if (!ifs.is_open()) {
+			cout << "openfile out error" << endl;
+		}
+		ifs.get(buf, sizeof(buf), '\0');
+		//从文件中读取字符到字符串buf，当遇到字符'\0'或读取了sizeof(buf)个字符时终止。
+		ifs.close();
+		if (strlen(buf) > 0)//读取到数据,说明程序有输出
+		{
+			cout << strlen(buf) << endl;
+			bufferevent_write(bev, buf, strlen(buf));//回传程序输出
+			return 0;
+		}
+		return -1;
+	}
 	return -1;
 }
